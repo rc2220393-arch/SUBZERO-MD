7b0e97b3d7ffbdb15afba00b4c6efa74f95eac79
rc2220393-arch <rc2220393@gmail.com>
2025-12-31 01:09:05 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~YdQVUI", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "923462297813", // Put Owner Number Here 
+  PREFIX: "https://mrfrankk-cdn.hf.space/media/HAMZA-KING-458.jpg", // Default Prefix You Can Change To .,*!?+-&%#@
+  CDN: "https://mrfrankk-cdn.hf.space/media/HAMZA-KING-458.jpg" // Dont change this part
 };
 
 // CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
